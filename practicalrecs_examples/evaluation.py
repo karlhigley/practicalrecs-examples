56d69f2442f8943e531d1c4d1eabce334ae951a6
Karl Higley <kmhigley@gmail.com>
2021-04-26 18:41:48 -0400

    import sorting in evaluation.py

--- practicalrecs_examples/evaluation.py
+++ practicalrecs_examples/evaluation.py
@@ -4,8 +4,8 @@ import faiss
 import numpy as np
 import torch as th
 
-from practicalrecs_examples.candidates import build_ann_index
 from practicalrecs_examples.artifacts import ArtifactRegistry
+from practicalrecs_examples.candidates import build_ann_index
 from practicalrecs_examples.filtering import build_bloom_filters
 from practicalrecs_examples.matrix_factorization import load_model
 from practicalrecs_examples.metrics import MetricsCalculator
